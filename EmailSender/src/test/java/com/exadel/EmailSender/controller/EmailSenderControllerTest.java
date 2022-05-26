155de4e6c7f4ddecf95d0c2aad80d05cd479df13
skambarov <saidaminqambarov408@gmail.com>
2022-05-26 17:43:59 +0300

    adding docker-compose.yml and Dockerfile and fully implemented

--- EmailSender/src/test/java/com/exadel/EmailSender/controller/EmailSenderControllerTest.java
+++ EmailSender/src/test/java/com/exadel/EmailSender/controller/EmailSenderControllerTest.java
@@ -2,10 +2,15 @@ package com.exadel.EmailSender.controller;
 
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 import com.exadel.EmailSender.dto.EmailDto;
 import com.exadel.EmailSender.service.EmailSender;
+import com.exadel.EmailSender.service.EmailService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +31,9 @@ class EmailSenderControllerTest {
 
     @Autowired
     private EmailSenderController emailSenderController;
+
+    @MockBean
+    private EmailService emailService;
 
     @Test
     void testSendEmailToAllUsers() throws Exception {
@@ -61,6 +69,95 @@ class EmailSenderControllerTest {
                 .build()
                 .perform(requestBuilder)
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void testGet() throws Exception {
+        when(this.emailService.getEmail((Long) any())).thenReturn(new EmailDto());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/email/send/get/{id}", 123L);
+        MockMvcBuilders.standaloneSetup(this.emailSenderController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("{\"userId\":null,\"subject\":null,\"text\":null}"));
+    }
+
+    @Test
+    void testGet2() throws Exception {
+        when(this.emailService.getEmail((Long) any())).thenReturn(new EmailDto());
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/email/send/get/{id}", 123L);
+        MockMvcBuilders.standaloneSetup(this.emailSenderController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("{\"userId\":null,\"subject\":null,\"text\":null}"));
+    }
+
+
+    @Test
+    void testGetAll() throws Exception {
+        when(this.emailService.getAllEmails()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/email/send/getAll");
+        MockMvcBuilders.standaloneSetup(this.emailSenderController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void testGetAll2() throws Exception {
+        when(this.emailService.getAllEmails()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/email/send/getAll");
+        MockMvcBuilders.standaloneSetup(this.emailSenderController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        doNothing().when(this.emailService).deleteEmail((Long) any());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/email/send/delete/{id}", 123L);
+        MockMvcBuilders.standaloneSetup(this.emailSenderController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void testDelete2() throws Exception {
+        doNothing().when(this.emailService).deleteEmail((Long) any());
+        MockHttpServletRequestBuilder deleteResult = MockMvcRequestBuilders.delete("/email/send/delete/{id}", 123L);
+        MockMvcBuilders.standaloneSetup(this.emailSenderController)
+                .build()
+                .perform(deleteResult)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        when(this.emailService.updateEmail((Long) any(), (EmailDto) any())).thenReturn(new EmailDto());
+
+        EmailDto emailDto = new EmailDto();
+        emailDto.setSubject("Hello from the Dreaming Spires");
+        emailDto.setText("Text");
+        emailDto.setUserId(123L);
+        String content = (new ObjectMapper()).writeValueAsString(emailDto);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/email/send/update/{id}", 123L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(this.emailSenderController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string("{\"userId\":null,\"subject\":null,\"text\":null}"));
     }
 }
 
