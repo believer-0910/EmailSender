5de03e389cce16ef8e710eb29ec5b646e09aee88
skambarov <saidaminqambarov408@gmail.com>
2022-06-29 17:50:07 +0300

    adding @ConfigurationProperties

--- EmailSender/src/main/java/com/exadel/EmailSender/service/UserService.java
+++ EmailSender/src/main/java/com/exadel/EmailSender/service/UserService.java
@@ -1,6 +1,7 @@
 package com.exadel.EmailSender.service;
 
 import com.exadel.EmailSender.dto.UserDto;
+import com.exadel.EmailSender.properties.GlobalUrlProperties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
@@ -8,23 +9,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
-import static com.exadel.EmailSender.utils.HotelMainUtils.GET_ALL_USERS;
-import static com.exadel.EmailSender.utils.HotelMainUtils.GET_USER_BY_ID;
-
 @Service
 public class UserService {
 
     private static final Logger log = LogManager.getLogger(UserService.class);
     private final WebClient.Builder webClientBuilder;
+    private final GlobalUrlProperties globalUrlProperties;
 
-    public UserService(WebClient.Builder webClientBuilder) {
+    public UserService(WebClient.Builder webClientBuilder, GlobalUrlProperties globalUrlProperties) {
         this.webClientBuilder = webClientBuilder;
+        this.globalUrlProperties = globalUrlProperties;
     }
 
     public List<UserDto> getAllUsers() {
+        String get_all_users = globalUrlProperties.getGet_all_users();
         List<UserDto> userDtoList = webClientBuilder.build()
                 .get()
-                .uri(GET_ALL_USERS)
+                .uri(get_all_users)
                 .retrieve()
                 .bodyToFlux(UserDto.class)
                 .doOnError(e -> log.error("Error while getting all users", e))
@@ -38,9 +39,10 @@ public class UserService {
     }
 
     public UserDto getUser(Long id) {
+        String get_user_by_id = globalUrlProperties.getGet_user_by_id();
         UserDto userDto = webClientBuilder.build()
                 .get()
-                .uri(GET_USER_BY_ID + id)
+                .uri(get_user_by_id + id)
                 .retrieve()
                 .bodyToMono(UserDto.class)
                 .doOnError(e -> log.error("User not found"))
